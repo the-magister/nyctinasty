8782126d939f0875d08a0421feabd57a2a9964eb
the-magister <magister@the-magister.com>
2017-11-02 09:15:01 -0700

    tinkered with ADC device

--- libraries/Skein_Messages/Skein_Messages.h
+++ libraries/Skein_Messages/Skein_Messages.h
@@ -18,15 +18,24 @@ typedef struct {
 	uint16_t dist[N_SENSOR];
 } SensorReading;
 
+// with a default
+const SensorReading defaultSensorReading = {
+	0, 2500,
+	1500,
+	2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500
+};
+
 // common command structure; commited to EEPROM and recalled at startup.
 typedef struct {
 	// send fps, frame = SensorReading
 	uint8_t fps;
 } Command;
 
+// with a default 
+const Command defaultCommand = { 20 };
+
 // save/load to/from EEPROM
 void saveCommand(Command command);
 void loadCommand(Command command);
-
 
 #endif
--- src/Sensor_Sharp_ADC/Sensor_Sharp_ADC.ino
+++ src/Sensor_Sharp_ADC/Sensor_Sharp_ADC.ino
@@ -5,41 +5,22 @@
 #include <EEPROM.h>
 #include "Skein_Messages.h"
 
-// poll the analog pins every interval
-Metro updateWhile(50UL);
+// poll the analog pins for this long
+unsigned long sensorPollTime = 50UL; // ms
 
 // store readings
-SensorReading reading;
+SensorReading reading = defaultSensorReading;
 
 // store settings
-Command settings;
+Command settings = defaultCommand;
 
 // set reading threshold; readings lower than this are oor
-const unsigned long threshold3000mm = 86;
-const unsigned long threshold2500mm = 102;
-const unsigned long threshold2000mm = 127;
-const unsigned long threshold1500mm = 167;
-const unsigned long threshold1000mm = 245;
-const unsigned long threshold500mm = 454;
-const unsigned long outOfRangeReading = threshold2500mm;
+const unsigned long reading3000mm = 86;
+const unsigned long reading2500mm = 102;
+const unsigned long reading2000mm = 127;
+const unsigned long oorReading = reading3000mm;
 
-// convert oor reading to oor distance
-const unsigned long magicNumberSlope = 266371;
-const unsigned long magicNumberIntercept = 87;
-const unsigned long outOfRangeDistance = magicNumberSlope / (unsigned long)outOfRangeReading - magicNumberIntercept;
-const unsigned long minDistance = 0;
-
-// defines for setting and clearing register bits
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
-
-// send string
-String message;
-
+// for comms
 SoftwareSerial mySerial(3, 2); // RX, TX; cross to other pair.
 
 void setup() {
@@ -51,126 +32,101 @@ void setup() {
 
   // put your setup code here, to run once:
   analogReference(EXTERNAL); // tune to 2.75V
+  // has 32K built-in resistor.
+  // connect AREF to +3.3v via 6.4 kOhm resistor
+  // if 6.2 kOhm used, 1023 reading is 2.76V
+  // if 6.8 kOhm used, 1023 reading is 2.72V
 
-  // Could have a message "3000," eight times and a terminator
-  message.reserve((4 + 1) * 8 + 1);
-
-  // ADC prescalar
-  // see: http://forum.arduino.cc/index.php?topic=6549.0
-  // set prescale to 128
-  sbi(ADCSRA, ADPS2);
-  sbi(ADCSRA, ADPS1);
-  sbi(ADCSRA, ADPS0);
-  // prescalar 16
-  //  sbi(ADCSRA,ADPS2);
-  //  cbi(ADCSRA,ADPS1);
-  //  cbi(ADCSRA,ADPS0);
-
-  // load settings
-  settings.fps = 20;
-  saveCommand(settings);
-  loadCommand(settings);
   // reset the interval
-  updateWhile.interval(1000UL/settings.fps);
-  Serial << "Startup. Target fps=" << settings.fps << endl;
-  Serial << "Startup. Update interval (ms)=" << 1000/settings.fps << endl;
-      
+  sensorPollTime = 1000UL / settings.fps;
+  Serial << F("Startup. Target fps=") << settings.fps << endl;
+  Serial << F("Startup. Sensor poll time (ms)=") << sensorPollTime << endl;
+
   // save reading information
   reading.min = 0;
-  reading.max = outOfRangeDistance;
-  reading.noise = 0.1 * float(outOfRangeDistance);
+  reading.max = readingToDistance(oorReading);
+  reading.noise = readingToDistance(reading2000mm);
 }
 
 void loop() {
+  // record the time
+  unsigned long tic = millis();
+  
   // poll sensors
   readSensors();
 
   // we have missing sensors
-  reading.dist[4] = outOfRangeReading;
-  reading.dist[5] = outOfRangeReading;
-  reading.dist[6] = outOfRangeReading;
-  reading.dist[7] = outOfRangeReading;
+  reading.dist[4] = reading.max;
+  reading.dist[5] = reading.max;
+  reading.dist[6] = reading.max;
+  reading.dist[7] = reading.max;
 
-  // PRINT the readings
-  for ( byte i = 0; i < N_SENSOR; i++ ) {
-    Serial << reading.dist[i]  << ',';
-  }
-  Serial << 0 << ',' << 1023;
-  Serial << endl;
+  // print the readings
+  printRanges();
 
-  // translate readings
-  calculateRanges();
-
-  // we have missing sensors
-  reading.dist[4] = outOfRangeDistance;
-  reading.dist[5] = outOfRangeDistance;
-  reading.dist[6] = outOfRangeDistance;
-  reading.dist[7] = outOfRangeDistance;
-
-
-  // check for settings
-//  checkCommands();
-  
   // send message
   sendRanges();
+
+  // do we need to adjust the poll time to get our target fps?
+  unsigned long elapsed = millis() - tic;
+  if( elapsed > (1000UL / settings.fps)+2 ) {
+    sensorPollTime--;
+    Serial << F("Adjusting sensor poll time down: ") << sensorPollTime << endl;
+  } else if( elapsed < (1000UL / settings.fps)-2 ) {
+    sensorPollTime++;
+    Serial << F("Adjusting sensor poll time up: ") << sensorPollTime << endl;    
+  }
 }
 
 void readSensors() {
-  // reset timer
-  updateWhile.reset();
-  word updates = 0;
-  static unsigned long smoothing = 10;
+  // create a timer
+  Metro updateWhile(sensorPollTime);
 
+  unsigned long updates = 0;
+  static uint32_t smoothing = 100;
+  static uint32_t values[] = {oorReading,oorReading,oorReading,oorReading,oorReading,oorReading,oorReading,oorReading};
+  
   // get a bunch of readings
   while ( !updateWhile.check() ) {
     for ( byte i = 0; i < N_SENSOR; i++ ) {
       unsigned long r = analogRead(i);
-      r = r < outOfRangeReading ? outOfRangeReading : r;
-      reading.dist[i] = ((unsigned long)reading.dist[i] * (smoothing - 1) + r) / smoothing;
+      r = r < oorReading ? oorReading : r;
+      values[i] = (values[i] * (smoothing - 1) + r) / smoothing;
     }
     updates ++;
   }
   smoothing = updates;
-}
 
-void calculateRanges() {
-  // calculate ranges
+  // convert to ranges
   for ( byte i = 0; i < N_SENSOR; i++ ) {
-    reading.dist[i] = magicNumberSlope / reading.dist[i] - magicNumberIntercept;
+    reading.dist[i] = readingToDistance(values[i]);
   }
+  
 }
 
-/*
-void checkCommands() {
-
-  // check for commands
-  if( ETin.receiveData() ) {
-    // reset the interval
-    updateWhile.interval(1000UL/settings.fps);
-    
-    Serial << "Command. Target fps=" << settings.fps << endl;
-    Serial << "Command. Update interval (ms)=" << 1000/settings.fps << endl;
-
-    // store this.
-    saveCommand(settings);
-  }
-   
+uint16_t readingToDistance( uint32_t reading ) {
+  const uint32_t magicNumberSlope = 266371;
+  const uint32_t magicNumberIntercept = 87;
+  // prevent div0 or exceeding 16 bits of information
+  reading = reading < oorReading ? oorReading : reading;
+  
+  uint32_t distance = magicNumberSlope / (uint32_t)reading - magicNumberIntercept;
+  return( distance );
 }
-*/
 
+// print ranges
+void printRanges() {
+  const char sep = ',';
+  
+  for ( byte i = 0; i < N_SENSOR; i++ ) {
+    Serial << reading.dist[i]  << sep;
+  }
+  Serial << reading.min << sep << reading.max << sep << reading.noise;
+  Serial << endl;
+}
 
 void sendRanges() {
-/*
-  // send the readings
-  for ( byte i = 0; i < N_SENSOR; i++ ) {
-    Serial << reading.dist[i]  << ',';
-  }
-  Serial << reading.min << ',' << reading.max;
-  Serial << endl;
-*/
   mySerial.print( "MSG" );
-  mySerial.write( (byte*)&reading, sizeof(reading));
+  mySerial.write( (byte*)&reading, sizeof(reading) );
   mySerial.flush();
-  
-//  Serial << "wrote bytes to mySerial:" << sizeof(reading) << endl;
 }
